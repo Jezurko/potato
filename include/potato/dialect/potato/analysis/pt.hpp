a30b04edf97e1629b34c6c07e47c59e27bee7365
Robert Konicar <xkonicar@fi.muni.cz>
2024-02-09 13:46:07 +0100

    analysis: Add join to the initial lattice.

--- include/potato/dialect/potato/analysis/pt.hpp
+++ include/potato/dialect/potato/analysis/pt.hpp
@@ -3,17 +3,39 @@
 POTATO_RELAX_WARNINGS
 #include <mlir/Analysis/DataFlow/DenseAnalysis.h>
 #include <mlir/Interfaces/CallInterfaces.h>
+#include <llvm/ADT/DenseMap.h>
+#include <llvm/ADT/SetVector.h>
 POTATO_UNRELAX_WARNINGS
 
 namespace potato::analysis {
 
-struct PointsToLattice : mlir::dataflow::AbstractDenseLattice {
-    mlir::ChangeResult join(const mlir::dataflow::AbstractDenseLattice &rhs) override;
+using pt_map = llvm::DenseMap< mlir::Value, llvm::SetVector< mlir::Value > >;
+using change_result = mlir::ChangeResult;
+
+struct PointsToLattice : mlir::dataflow::AbstractDenseLattice
+{
+    pt_map pt_relation;
+
+    change_result merge(const PointsToLattice &rhs) {
+        change_result res = change_result::NoChange;
+        for (auto &[key, rhs_value] : rhs.pt_relation) {
+            auto &lhs_value = pt_relation[key];
+            if (lhs_value.set_union(rhs_value)) {
+                res |= change_result::Change;
+            }
+        }
+        return res;
+    }
+
+    change_result join(const mlir::dataflow::AbstractDenseLattice &rhs) override {
+        return this->merge(*static_cast< const PointsToLattice *>(&rhs));
+    };
 
     mlir::ChangeResult meet(const mlir::dataflow::AbstractDenseLattice &rhs) override;
 };
 
-struct PointsToAnalaysis : mlir::dataflow::DenseDataFlowAnalysis< PointsToLattice > {
+struct PointsToAnalaysis : mlir::dataflow::DenseDataFlowAnalysis< PointsToLattice >
+{
 
     void visitOperation(mlir::Operation *op, const PointsToLattice &before, PointsToLattice *after) override;
 
