d0448db2d17ef84734fc58520dc2e70d4c361696
Robert Konicar <xkonicar@fi.muni.cz>
2024-02-09 11:48:32 +0100

    tools:potato-opt: Initialize potato-opt.

---
+++ tools/potato-opt.cpp
@@ -0,0 +1,28 @@
+#include "potato/util/warnings.hpp"
+
+POTATO_RELAX_WARNINGS
+#include "mlir/IR/Dialect.h"
+#include "mlir/IR/MLIRContext.h"
+#include "mlir/InitAllDialects.h"
+#include "mlir/InitAllPasses.h"
+#include "mlir/Pass/Pass.h"
+#include "mlir/Tools/mlir-opt/MlirOptMain.h"
+POTATO_UNRELAX_WARNINGS
+
+#include "potato/dialect/potato/potato.hpp"
+
+int main(int argc, char **argv)
+{
+     mlir::registerAllPasses();
+    // Register potato passes here
+
+    mlir::DialectRegistry registry;
+    // register dialects
+    registry.insert< potato::pt::PotatoDialect >();
+    // POTATO register
+    mlir::registerAllDialects(registry);
+
+    return mlir::failed(
+        mlir::MlirOptMain(argc, argv, "PoTATo driver\n", registry)
+    );
+}
