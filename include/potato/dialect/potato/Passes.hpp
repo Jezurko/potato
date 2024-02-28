c00ec86c881b4f79d120ba00f59074f5cbf7f14d
Robert Konicar <xkonicar@fi.muni.cz>
2024-02-28 17:31:44 +0100

    treewide: Init analysis pass.

---
+++ include/potato/dialect/potato/Passes.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <potato/util/warnings.hpp>
+
+POTATO_RELAX_WARNINGS
+#include <mlir/IR/Operation.h>
+#include <mlir/Pass/Pass.h>
+#include <mlir/Pass/PassManager.h>
+POTATO_UNRELAX_WARNINGS
+
+#include <memory>
+
+namespace potato::pt {
+
+    std::unique_ptr< mlir::Pass > createPointsToPass();
+
+    #define GEN_PASS_REGISTRATION
+    #include "potato/dialect/potato/Passes.h.inc"
+
+
+} // namespace potato::pt
