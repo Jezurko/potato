e1a20ebe6ad59d178480276bda22b50fa0951552
Robert Konicar <xkonicar@fi.muni.cz>
2024-07-18 16:40:29 +0200

    analysis: Rework var/allocation naming to keep names when revisiting.

--- lib/analysis/pt.cpp
+++ lib/analysis/pt.cpp
@@ -2,22 +2,33 @@
 
 namespace potato::analysis {
 
+unsigned int aa_lattice::variable_count = 0;
 unsigned int aa_lattice::mem_loc_count = 0;
-unsigned int aa_lattice::constant_count = 0;
+
+unsigned int aa_lattice::var_count() {
+    return variable_count++;
+}
 
 unsigned int aa_lattice::alloc_count() {
     return mem_loc_count++;
 }
 
-unsigned int aa_lattice::const_count() {
-    return constant_count++;
-}
-
-void aa_lattice::print(llvm::raw_ostream &os) const
-{
+void aa_lattice::print(llvm::raw_ostream &os) const {
     for (const auto &[key, vals] : pt_relation) {
         os << key << " -> " << vals;
     }
+}
+
+std::string aa_lattice::get_var_name() {
+    if (!var_name)
+        var_name = "var" + std::to_string(var_count());
+    return var_name.value();
+}
+
+std::string aa_lattice::get_alloc_name() {
+    if (!alloc_name)
+        alloc_name = "mem_alloc" + std::to_string(alloc_count());
+    return alloc_name.value();
 }
 
 void print_analysis_result(mlir::DataFlowSolver &solver, mlir_operation *op, llvm::raw_ostream &os)
