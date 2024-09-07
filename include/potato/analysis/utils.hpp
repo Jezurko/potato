70cc9573336e34a9472b1c8367feb3628783b98e
Robert Konicar <xkonicar@fi.muni.cz>
2024-09-07 19:22:03 +0200

    util: Change formating.

--- include/potato/analysis/utils.hpp
+++ include/potato/analysis/utils.hpp
@@ -18,65 +18,65 @@ namespace potato::util {
 }
 
 namespace potato::analysis {
-template < typename value_t, template< typename >typename set_t >
-using pt_map = llvm::DenseMap< value_t, set_t< value_t > >;
+    template < typename value_t, template< typename >typename set_t >
+    using pt_map = llvm::DenseMap< value_t, set_t< value_t > >;
 
-// Allows using DenseSet with above template without having to specify default args
-template< typename value_t >
-using dense_set = llvm::DenseSet< value_t >;
+    // Allows using DenseSet with above template without having to specify default args
+    template< typename value_t >
+    using dense_set = llvm::DenseSet< value_t >;
 
-struct pt_element
-{
-    mlir_value val;
-    std::string name;
+    struct pt_element
+    {
+        mlir_value val;
+        std::string name;
 
-    bool operator==(const pt_element &rhs) const {
-        if (val || rhs.val)
-            return val == rhs.val;
-        return name == rhs.name;
+        bool operator==(const pt_element &rhs) const {
+            if (val || rhs.val)
+                return val == rhs.val;
+            return name == rhs.name;
+        }
+
+        void print(llvm::raw_ostream &os) const { os << name << ": " << val; };
+    };
+
+    auto get_args(ppoint &point) -> mlir_block::BlockArgListType;
+
+    bool sets_intersect(const auto &lhs, const auto &rhs) {
+        for (const auto &lhs_elem : lhs) {
+            if (rhs.contains(lhs_elem))
+               return true;
+        }
+        return false;
     }
-
-    void print(llvm::raw_ostream &os) const { os << name << ": " << val; };
-};
-
-auto get_args(ppoint &point) -> mlir_block::BlockArgListType;
-
-bool sets_intersect(const auto &lhs, const auto &rhs) {
-    for (const auto &lhs_elem : lhs) {
-        if (rhs.contains(lhs_elem))
-           return true;
-    }
-    return false;
-}
 } // namespace potato::analysis
 
 
 namespace llvm {
-using potato::analysis::pt_element;
+    using potato::analysis::pt_element;
 
-inline raw_ostream &operator <<(raw_ostream &os, const pt_element &value) {
-    value.print(os);
-    return os;
-}
-
-template<>
-struct DenseMapInfo< pt_element > {
-    using value_info = DenseMapInfo< mlir_value >;
-    static inline pt_element getEmptyKey() {
-        return {value_info::getEmptyKey(), ""};
+    inline raw_ostream &operator <<(raw_ostream &os, const pt_element &value) {
+        value.print(os);
+        return os;
     }
 
-    static inline pt_element getTombstoneKey() {
-        return {value_info::getTombstoneKey(), ""};
-    }
+    template<>
+    struct DenseMapInfo< pt_element > {
+        using value_info = DenseMapInfo< mlir_value >;
+        static inline pt_element getEmptyKey() {
+            return {value_info::getEmptyKey(), ""};
+        }
 
-    static unsigned getHashValue(const pt_element &val) {
-        return val.val ? value_info::getHashValue(val.val) : (unsigned) llvm::hash_value(val.name);
-    }
+        static inline pt_element getTombstoneKey() {
+            return {value_info::getTombstoneKey(), ""};
+        }
 
-    static bool isEqual(const pt_element &lhs, const pt_element &rhs) {
-        return lhs == rhs;
-    }
-};
+        static unsigned getHashValue(const pt_element &val) {
+            return val.val ? value_info::getHashValue(val.val) : (unsigned) llvm::hash_value(val.name);
+        }
+
+        static bool isEqual(const pt_element &lhs, const pt_element &rhs) {
+            return lhs == rhs;
+        }
+    };
 
 } // namespace llvm
