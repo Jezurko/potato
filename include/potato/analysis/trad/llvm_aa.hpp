bdc0657b85aa37d334979be76a1bbfb44fb53446
Robert Konicar <xkonicar@fi.muni.cz>
2024-11-12 16:53:20 +0100

    trad:aa: Update to match pt.

--- include/potato/analysis/trad/llvm_aa.hpp
+++ include/potato/analysis/trad/llvm_aa.hpp
@@ -7,8 +7,11 @@ POTATO_RELAX_WARNINGS
 #include <mlir/Dialect/LLVMIR/LLVMDialect.h>
 POTATO_UNRELAX_WARNINGS
 
+#include "potato/analysis/andersen.hpp"
 #include "potato/analysis/lattice.hpp"
 #include "potato/analysis/utils.hpp"
+
+#include <memory>
 
 // This is a simple Andersen-style alias analysis implementation for llvm dialect
 // It's main purpose is to be used for quick testing (or comparsion)
@@ -51,37 +54,41 @@ namespace {
     namespace mllvm = mlir::LLVM;
 }
 
-struct llvm_andersen : mlir::dataflow::DenseForwardDataFlowAnalysis< llaa_lattice >{
-    using base = mlir::dataflow::DenseForwardDataFlowAnalysis< llaa_lattice >;
+struct llvm_andersen : mlir::dataflow::DenseForwardDataFlowAnalysis< aa_lattice >{
+    using base = mlir::dataflow::DenseForwardDataFlowAnalysis< aa_lattice >;
     using base::base;
 
     using base::propagateIfChanged;
 
-    void visit_op(mllvm::AllocaOp &op, const llaa_lattice &before, llaa_lattice *after);
-    void visit_op(mllvm::StoreOp &op, const llaa_lattice &before, llaa_lattice *after);
-    void visit_op(mllvm::LoadOp &op, const llaa_lattice &before, llaa_lattice *after);
-    void visit_op(mllvm::ConstantOp &op, const llaa_lattice &before, llaa_lattice *after);
-    void visit_op(mllvm::ZeroOp &op, const llaa_lattice &before, llaa_lattice *after);
-    void visit_op(mllvm::GEPOp &op, const llaa_lattice &before, llaa_lattice *after);
-    void visit_op(mllvm::AddressOfOp &op, const llaa_lattice &before, llaa_lattice *after);
-    void visit_op(mllvm::SExtOp &op, const llaa_lattice &before, llaa_lattice *after);
-    void visit_op(mllvm::GlobalOp &op, const llaa_lattice &before, llaa_lattice *after);
-    void visit_op(mllvm::MemcpyOp &op, const llaa_lattice &before, llaa_lattice *after);
-    void visit_op(mllvm::SelectOp &op, const llaa_lattice &before, llaa_lattice *after);
+    void visit_op(mllvm::AllocaOp &op, const aa_lattice &before, aa_lattice *after);
+    void visit_op(mllvm::StoreOp &op, const aa_lattice &before, aa_lattice *after);
+    void visit_op(mllvm::LoadOp &op, const aa_lattice &before, aa_lattice *after);
+    void visit_op(mllvm::ConstantOp &op, const aa_lattice &before, aa_lattice *after);
+    void visit_op(mllvm::ZeroOp &op, const aa_lattice &before, aa_lattice *after);
+    void visit_op(mllvm::GEPOp &op, const aa_lattice &before, aa_lattice *after);
+    void visit_op(mllvm::AddressOfOp &op, const aa_lattice &before, aa_lattice *after);
+    void visit_op(mllvm::SExtOp &op, const aa_lattice &before, aa_lattice *after);
+    void visit_op(mllvm::GlobalOp &op, const aa_lattice &before, aa_lattice *after);
+    void visit_op(mllvm::MemcpyOp &op, const aa_lattice &before, aa_lattice *after);
+    void visit_op(mllvm::SelectOp &op, const aa_lattice &before, aa_lattice *after);
 
-    void visit_op(mlir::BranchOpInterface &op, const llaa_lattice &before, llaa_lattice *after);
+    void visit_op(mlir::BranchOpInterface &op, const aa_lattice &before, aa_lattice *after);
 
-    void visit_cmp(mlir::Operation *op, const llaa_lattice &before, llaa_lattice *after);
-    void visit_arith(mlir::Operation *op, const llaa_lattice &before, llaa_lattice *after);
+    void visit_cmp(mlir::Operation *op, const aa_lattice &before, aa_lattice *after);
+    void visit_arith(mlir::Operation *op, const aa_lattice &before, aa_lattice *after);
     std::vector< mlir::Operation * > get_function_returns(mlir::FunctionOpInterface func);
-    std::vector< const llaa_lattice * > get_or_create_for(mlir::Operation * dep, const std::vector< mlir::Operation * > &ops);
+    std::vector< const aa_lattice * > get_or_create_for(mlir::Operation * dep, const std::vector< mlir::Operation * > &ops);
 
-    void visitOperation(mlir::Operation *op, const llaa_lattice &before, llaa_lattice *after) override;
+    void visitOperation(mlir::Operation *op, const aa_lattice &before, aa_lattice *after) override;
     void visitCallControlFlowTransfer(mlir::CallOpInterface call,
                                       mlir::dataflow::CallControlFlowAction action,
-                                      const llaa_lattice &before,
-                                      llaa_lattice *after) override;
-    void setToEntryState(llaa_lattice *lattice) override;
+                                      const aa_lattice &before,
+                                      aa_lattice *after) override;
+    void setToEntryState(aa_lattice *lattice) override;
+    mlir::LogicalResult initialize(mlir_operation *op) override;
+
+    private:
+    std::shared_ptr< aa_lattice::relation_t > relation;
 };
 
 void print_analysis_result(mlir::DataFlowSolver &solver, mlir_operation *op, llvm::raw_ostream &os);
