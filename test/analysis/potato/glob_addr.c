9edcdc0b731b164938a49d3b8b7ce5ecbfbf1308
Robert Konicar <xkonicar@fi.muni.cz>
2024-11-19 19:15:03 +0100

    test: Simple global noalias test.

---
+++ test/analysis/potato/glob_addr.c
@@ -0,0 +1,10 @@
+// RUN: %emit-llvm -o - %s | %llvm-to-mlir -o - | %potato-opt --llvm-ir-to-potato --points-to-pass="print_lattice=true print_stats=false print_func_stats=false" -o /dev/null
+
+int a = 0, b = 1;
+
+void no_alias(int *p, int *q) { return; }
+int main() {
+    int *p = &a;
+    int *q = &b;
+    no_alias(p, q);
+}
