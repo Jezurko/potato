abac24111f2a541176a68152e1bd5be96d177e22
Robert Konicar <xkonicar@fi.muni.cz>
2024-11-19 19:14:14 +0100

    dialect: Fix global var symbols.

--- lib/dialect/potato.cpp
+++ lib/dialect/potato.cpp
@@ -2,11 +2,13 @@
 
 POTATO_RELAX_WARNINGS
 #include <mlir/IR/DialectImplementation.h>
+#include <mlir/Interfaces/FunctionInterfaces.h>
 POTATO_UNRELAX_WARNINGS
 
 #include "potato/dialect/potato.hpp"
 #include "potato/dialect/ops.hpp"
 #include "potato/dialect/types.hpp"
+#include "potato/util/common.hpp"
 
 namespace potato::pt
 {
@@ -31,6 +33,24 @@ namespace potato::pt
     {
         assert(false);
     }
+
+
+    logical_result AddressOp::verifySymbolUses(mlir::SymbolTableCollection &symbolTable) {
+        mlir_operation *module = (*this)->getParentOp();
+        while (module && !(module->hasTrait< mlir::OpTrait::SymbolTable >() &&
+                           module->hasTrait< mlir::OpTrait::IsIsolatedFromAbove >()))
+        {
+            module = module->getParentOp();
+        }
+        assert(module && "unexpected operation outside of a module");
+        mlir_operation *symbol = symbolTable.lookupSymbolIn(module, getSymbolAttr());
+
+        if (!symbol) {
+            return emitOpError("Invalid symbol.");
+        }
+        return logical_result::success();
+    }
+
 
 }
 
