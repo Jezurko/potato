ce837ccf3772ffd05e2a056e28af4329ae4f12b1
Robert Konicar <xkonicar@fi.muni.cz>
2024-11-20 17:08:46 +0100

    anlaysis:andersen: Move implementations to cpp.

--- include/potato/analysis/andersen.hpp
+++ include/potato/analysis/andersen.hpp
@@ -18,77 +18,20 @@ struct aa_lattice : mlir_dense_abstract_lattice {
     bool initialized() const { return (bool) pt_relation; }
     void initialize_with(std::shared_ptr< relation_t > &relation) { pt_relation = relation; }
 
-    // TODO: Probably replace most of the following functions with some custom API that doesn't introduce
-    //       so many random return values with iterators and stuff
+    const pointee_set *lookup(const elem_t &val) const;
+    pointee_set *lookup(const elem_t &val);
+    pointee_set *lookup(const mlir_value &val) { return lookup(elem_t(val)); }
 
-    const pointee_set *lookup(const elem_t &val) const {
-        if (!pt_relation)
-            return nullptr;
-        auto it = pt_relation->find(val);
-        if (it == pt_relation->end())
-            return nullptr;
-        return &it->second;
-    }
+    static elem_t new_func(mlir_operation *op) { return elem_t::make_func(op); }
+    static elem_t new_glob(mlir_operation *op) { return elem_t::make_glob(op); }
+    static pointee_set new_pointee_set() { return pointee_set(); }
+    static pointee_set new_top_set() { return pointee_set::make_top(); }
 
-    const pointee_set *lookup(const mlir_value &val) const {
-        return lookup(elem_t(val));
-    }
+    change_result join_empty(mlir_value val);
 
-    pointee_set *lookup(const elem_t &val) {
-        if (!pt_relation)
-            return nullptr;
-        auto it = pt_relation->find(val);
-        if (it == pt_relation->end())
-            return nullptr;
-        return &it->second;
-    }
-
-    pointee_set *lookup(const mlir_value &val) {
-        return lookup(elem_t(val));
-    }
-
-    static elem_t new_func(mlir_operation *op) {
-        return elem_t::make_func(op);
-    }
-
-    static elem_t new_glob(mlir_operation *op) {
-        return elem_t::make_glob(op);
-    }
-
-    static pointee_set new_pointee_set() {
-        return pointee_set();
-    }
-
-    static pointee_set new_top_set() {
-        return pointee_set::make_top();
-    }
-
-    change_result join_empty(mlir_value val) {
-        auto set = pointee_set();
-        auto inserted = pt_relation->insert({{val}, set});
-        if (inserted.second) {
-            return change_result::Change;
-        }
-        return change_result::NoChange;
-    }
-
-    change_result add_constant(mlir_value val) {
-        return join_empty(val);
-    }
-
-    change_result new_alloca(mlir_value val) {
-        auto set = pointee_set();
-        set.insert(elem_t::make_alloca(val.getDefiningOp()));
-        auto [it, inserted] = pt_relation->insert({{val}, set});
-        if (inserted)
-            return change_result::Change;
-        else
-            return join_var(val, set);
-
-    }
-
+    change_result add_constant(mlir_value val) { return join_empty(val); }
+    change_result new_alloca(mlir_value val);
     auto new_var(mlir_value var, const pointee_set& pt_set) {
-        assert(pt_relation);
         return pt_relation->insert({{var}, pt_set});
     }
 
@@ -103,6 +46,7 @@ struct aa_lattice : mlir_dense_abstract_lattice {
         return new_var(var, set);
     }
 
+    // TODO: remove
     change_result set_var(mlir_value val, const pointee_set &set) {
         auto [var, inserted] = new_var(val, set);
         if (inserted) {
@@ -116,6 +60,7 @@ struct aa_lattice : mlir_dense_abstract_lattice {
         return change_result::NoChange;
     }
 
+    // TODO: remove
     change_result set_var(mlir_value val, mlir_value pointee) {
         auto [var, inserted] = new_var(val, pointee);
         if (inserted)
@@ -131,6 +76,7 @@ struct aa_lattice : mlir_dense_abstract_lattice {
         return change_result::NoChange;
     }
 
+    // TODO: remove
     change_result set_var(elem_t elem, const pointee_set &set) {
         auto [var, inserted] = pt_relation->insert({elem, set});
         if (inserted)
@@ -142,83 +88,18 @@ struct aa_lattice : mlir_dense_abstract_lattice {
         return change_result::NoChange;
     }
 
-    change_result join_var(mlir_value val, mlir_value trg) {
-        auto val_pt  = lookup(val);
-        if (!val_pt) {
-            return set_var(val, elem_t::make_alloca(trg.getDefiningOp()));
-        }
-        return val_pt->join(pointee_set(elem_t::make_alloca(trg.getDefiningOp())));
-    }
-
-    change_result join_var(mlir_value val, const pointee_set &set) {
-        auto val_pt  = lookup(val);
-        if (!val_pt) {
-            return set_var(val, set);
-        }
-        return val_pt->join(set);
-    }
-
-    change_result join_var(elem_t elem, const pointee_set &set) {
-        auto val_pt  = lookup(elem);
-        if (!val_pt) {
-            return set_var(elem, set);
-        }
-        return val_pt->join(set);
-    }
-
-    change_result join_var(mlir_value val, const pointee_set *set) {
-        auto val_pt  = lookup(val);
-        if (!val_pt) {
-            return set_var(val, *set);
-        }
-        return val_pt->join(*set);
-    }
-
-    change_result join_var(elem_t elem, const pointee_set *set) {
-        auto val_pt  = lookup(elem);
-        if (!val_pt) {
-            return set_var(elem, *set);
-        }
-        return val_pt->join(*set);
-    }
+    change_result join_var(mlir_value val, mlir_value trg);
+    change_result join_var(mlir_value val, const pointee_set &set);
+    change_result join_var(elem_t elem, const pointee_set &set);
+    change_result join_var(mlir_value val, const pointee_set *set);
+    change_result join_var(elem_t elem, const pointee_set *set);
 
     // for each p in to lookup pts(p) and join it with from
-    change_result join_all_pointees_with(pointee_set *to, const pointee_set *from) {
-        auto changed = change_result::NoChange;
-        std::vector< const elem_t * > to_update;
-        for (auto &val : to->get_set_ref()) {
-            to_update.push_back(&val);
-        }
-        // This has to be done so that we don't change the set we are iterating over under our hands
-        for (auto &key : to_update) {
-            changed |= join_var(*key, from);
-        }
-
-        return changed;
-    }
-
+    change_result join_all_pointees_with(pointee_set *to, const pointee_set *from);
     // for each p in from do pts(to) join_with pts(p)
-    change_result copy_all_pts_into(elem_t to, const pointee_set *from) {
-        auto changed = change_result::NoChange;
-        std::vector< const pointee_set * > to_join;
+    change_result copy_all_pts_into(elem_t to, const pointee_set *from);
 
-        for (const auto &val : from->get_set_ref()) {
-            auto val_pt = lookup(val);
-            if (val_pt) {
-                to_join.push_back(val_pt);
-            }
-        }
-
-        // make sure `to` is in the lattice
-        changed |= join_var(to, pointee_set());
-
-        for (auto *join : to_join) {
-            changed |= join_var(to, join);
-        }
-
-        return changed;
-    }
-
+    // TODO: rework
     change_result set_all_unknown() {
         auto changed = change_result::NoChange;
         for (auto &[_, pt_set] : *pt_relation) {
@@ -227,42 +108,8 @@ struct aa_lattice : mlir_dense_abstract_lattice {
         return changed;
     }
 
-    change_result merge(const aa_lattice &rhs) {
-        if (pt_relation && !rhs.pt_relation)
-            return change_result::NoChange;
-        if (pt_relation && rhs.pt_relation == pt_relation) {
-            return change_result::NoChange;
-        }
-        if (pt_relation && rhs.pt_relation) {
-            llvm::errs() << "Merging two different relations.\n";
-            change_result res = change_result::NoChange;
-            for (const auto &[key, rhs_value] : *rhs.pt_relation) {
-                auto &lhs_value = (*pt_relation)[key];
-                res |= lhs_value.join(rhs_value);
-            }
-            return res;
-        }
-        if (rhs.pt_relation) {
-            pt_relation = rhs.pt_relation;
-            return change_result::Change;
-        }
-        assert(false);
-        return change_result::NoChange;
-    }
-
-    change_result intersect(const aa_lattice &rhs) {
-        if (rhs.pt_relation == pt_relation) {
-            return change_result::NoChange;
-        }
-        change_result res = change_result::NoChange;
-        for (const auto &[key, rhs_value] : *rhs.pt_relation) {
-            // non-existent entry would be considered top, so creating a new entry
-            // and intersecting it will create the correct value
-            auto &lhs_value = (*pt_relation)[key];
-            res |= lhs_value.meet(rhs_value);
-        }
-        return res;
-    }
+    change_result merge(const aa_lattice &rhs);
+    change_result intersect(const aa_lattice &rhs);
 
     change_result join(const mlir_dense_abstract_lattice &rhs) override {
         return this->merge(*static_cast< const aa_lattice *>(&rhs));
@@ -274,7 +121,12 @@ struct aa_lattice : mlir_dense_abstract_lattice {
 
     void print(llvm::raw_ostream &os) const override;
 
-    static void add_dependencies(mlir::Operation *op, mlir_dense_dfa< aa_lattice > *analysis, ppoint point, auto get_or_create) {
+    static void add_dependencies(
+            mlir::Operation *op,
+            mlir_dense_dfa< aa_lattice > *analysis,
+            ppoint point,
+            auto get_or_create
+    ) {
         for (auto arg : op->getOperands()) {
             aa_lattice *arg_state;
             if (auto def_op = arg.getDefiningOp()) {
--- lib/analysis/andersen.cpp
+++ lib/analysis/andersen.cpp
@@ -1,6 +1,155 @@
 #include "potato/analysis/andersen.hpp"
 namespace potato::analysis {
 
+const aa_lattice::pointee_set *aa_lattice::lookup(const elem_t &val) const {
+    if (!pt_relation)
+        return nullptr;
+    auto it = pt_relation->find(val);
+    if (it == pt_relation->end())
+        return nullptr;
+    return &it->second;
+}
+
+aa_lattice::pointee_set *aa_lattice::lookup(const elem_t &val) {
+    if (!pt_relation)
+        return nullptr;
+    auto it = pt_relation->find(val);
+    if (it == pt_relation->end())
+        return nullptr;
+    return &it->second;
+}
+
+change_result aa_lattice::join_empty(mlir_value val) {
+    auto set = pointee_set();
+    auto inserted = pt_relation->insert({{val}, set});
+    if (inserted.second) {
+        return change_result::Change;
+    }
+    return change_result::NoChange;
+}
+
+change_result aa_lattice::new_alloca(mlir_value val) {
+    auto set = pointee_set();
+    set.insert(elem_t::make_alloca(val.getDefiningOp()));
+    auto [it, inserted] = pt_relation->insert({{val}, set});
+    if (inserted)
+        return change_result::Change;
+    else
+        return join_var(val, set);
+}
+
+change_result aa_lattice::join_var(mlir_value val, mlir_value trg) {
+    auto val_pt  = lookup(val);
+    if (!val_pt) {
+        return set_var(val, elem_t::make_alloca(trg.getDefiningOp()));
+    }
+    return val_pt->join(pointee_set(elem_t::make_alloca(trg.getDefiningOp())));
+}
+
+change_result aa_lattice::join_var(mlir_value val, const pointee_set &set) {
+    auto val_pt  = lookup(val);
+    if (!val_pt) {
+        return set_var(val, set);
+    }
+    return val_pt->join(set);
+}
+
+change_result aa_lattice::join_var(elem_t elem, const pointee_set &set) {
+    auto val_pt  = lookup(elem);
+    if (!val_pt) {
+        return set_var(elem, set);
+    }
+    return val_pt->join(set);
+}
+
+change_result aa_lattice::join_var(mlir_value val, const pointee_set *set) {
+    auto val_pt  = lookup(val);
+    if (!val_pt) {
+        return set_var(val, *set);
+    }
+    return val_pt->join(*set);
+}
+
+change_result aa_lattice::join_var(elem_t elem, const pointee_set *set) {
+    auto val_pt  = lookup(elem);
+    if (!val_pt) {
+        return set_var(elem, *set);
+    }
+    return val_pt->join(*set);
+}
+
+change_result aa_lattice::join_all_pointees_with(pointee_set *to, const pointee_set *from) {
+    auto changed = change_result::NoChange;
+    std::vector< const elem_t * > to_update;
+    for (auto &val : to->get_set_ref()) {
+        to_update.push_back(&val);
+    }
+    // This has to be done so that we don't change the set we are iterating over under our hands
+    for (auto &key : to_update) {
+        changed |= join_var(*key, from);
+    }
+
+    return changed;
+}
+
+change_result aa_lattice::copy_all_pts_into(elem_t to, const pointee_set *from) {
+    auto changed = change_result::NoChange;
+    std::vector< const pointee_set * > to_join;
+
+    for (const auto &val : from->get_set_ref()) {
+        auto val_pt = lookup(val);
+        if (val_pt) {
+            to_join.push_back(val_pt);
+        }
+    }
+
+    // make sure `to` is in the lattice
+    changed |= join_var(to, pointee_set());
+
+    for (auto *join : to_join) {
+        changed |= join_var(to, join);
+    }
+
+    return changed;
+}
+
+change_result aa_lattice::merge(const aa_lattice &rhs) {
+    if (pt_relation && !rhs.pt_relation)
+        return change_result::NoChange;
+    if (pt_relation && rhs.pt_relation == pt_relation) {
+        return change_result::NoChange;
+    }
+    if (pt_relation && rhs.pt_relation) {
+        llvm::errs() << "Merging two different relations.\n";
+        change_result res = change_result::NoChange;
+        for (const auto &[key, rhs_value] : *rhs.pt_relation) {
+            auto &lhs_value = (*pt_relation)[key];
+            res |= lhs_value.join(rhs_value);
+        }
+        return res;
+    }
+    if (rhs.pt_relation) {
+        pt_relation = rhs.pt_relation;
+        return change_result::Change;
+    }
+    assert(false);
+    return change_result::NoChange;
+}
+
+change_result aa_lattice::intersect(const aa_lattice &rhs) {
+    if (rhs.pt_relation == pt_relation) {
+        return change_result::NoChange;
+    }
+    change_result res = change_result::NoChange;
+    for (const auto &[key, rhs_value] : *rhs.pt_relation) {
+        // non-existent entry would be considered top, so creating a new entry
+        // and intersecting it will create the correct value
+        auto &lhs_value = (*pt_relation)[key];
+        res |= lhs_value.meet(rhs_value);
+    }
+    return res;
+}
+
 void aa_lattice::print(llvm::raw_ostream &os) const {
     auto sep = "";
     for (const auto &[key, vals] : *pt_relation) {
