f6e4a0da9ea5ecbfc3051d09c279f2cd6109708f
Robert Konicar <xkonicar@fi.muni.cz>
2024-11-22 21:58:56 +0100

    test: Add test with global struct.

---
+++ test/analysis/potato/global-call-struct-modified.c
@@ -0,0 +1,31 @@
+// RUN: %emit-llvm -o - %s | %llvm-to-mlir -o - | %potato-opt --llvm-ir-to-potato --points-to-pass="print_lattice=true print_stats=false print_func_stats=false" -o /dev/null
+// RUN: %emit-llvm -o - %s | %llvm-to-mlir -o - | %potato-opt --llvm-ir-to-potato --canonicalize="region-simplify=disabled" --points-to-pass="print_lattice=true print_stats=false print_func_stats=false" -o /dev/null
+// RUN: %emit-llvm -o - %s | %llvm-to-mlir -o - | %potato-opt --llvm-ir-to-potato --steensgaard-points-to-pass="print_lattice=true print_stats=false print_func_stats=false" -o /dev/null
+// RUN: %emit-llvm -o - %s | %llvm-to-mlir -o - | %potato-opt --llvm-ir-to-potato --canonicalize="region-simplify=disabled" --steensgaard-points-to-pass="print_lattice=true print_stats=false print_func_stats=false" -o /dev/null
+
+void may_alias(int *p, int *q) { return; }
+void must_alias(int *p, int *q) { return; }
+
+struct MyStruct {
+	char f0[20];
+	int f1;
+	int* f2;
+};
+
+int x, y;
+struct MyStruct global = {"abcdefg", 20, &x};
+
+void bar(int** pp, int** qq) {
+	*pp = &x;
+	*qq = &x;
+}
+
+int main() {
+	int *p, *q;
+	int **pp, **qq;
+	pp = &p;
+	qq = &q;
+	bar(pp,qq);
+	must_alias(p, q);
+	must_alias(global.f2, *qq);
+}
