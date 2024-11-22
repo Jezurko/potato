876bbd859e68b7ad93ee9d755d69e5b391242dfb
Robert Konicar <xkonicar@fi.muni.cz>
2024-11-22 22:19:26 +0100

    test: Add more globals tests.

---
+++ test/analysis/basic_c_tests/global-initializer.c
@@ -0,0 +1,34 @@
+/*
+ * Global variable
+ * Author: Sen Ye
+ * Date: 13/10/2013
+ * Description: initialise global variables when declared
+ *		and check alias in main function.
+ */
+
+// RUN: %emit-llvm -o - %s | %llvm-to-mlir -o - | %potato-opt --llvm-ir-to-potato --points-to-pass="print_lattice=true print_stats=false print_func_stats=false" -o /dev/null
+// RUN: %emit-llvm -o - %s | %llvm-to-mlir -o - | %potato-opt --llvm-ir-to-potato --canonicalize="region-simplify=disabled" --points-to-pass="print_lattice=true print_stats=false print_func_stats=false" -o /dev/null
+// RUN: %emit-llvm -o - %s | %llvm-to-mlir -o - | %potato-opt --llvm-ir-to-potato --steensgaard-points-to-pass="print_lattice=true print_stats=false print_func_stats=false" -o /dev/null
+// RUN: %emit-llvm -o - %s | %llvm-to-mlir -o - | %potato-opt --llvm-ir-to-potato --canonicalize="region-simplify=disabled" --steensgaard-points-to-pass="print_lattice=true print_stats=false print_func_stats=false" -o /dev/null
+
+#include "aliascheck.h"
+
+int x;
+int *p, *q;
+int **pp = &p;
+int **qq = &q;
+
+void foo() {
+	p = &x;
+}
+
+void bar() {
+	q = &x;
+}
+
+int main() {
+	must_alias(*pp, *qq);
+	foo();
+	bar();
+	return 0;
+}
