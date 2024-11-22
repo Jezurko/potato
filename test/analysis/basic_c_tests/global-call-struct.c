b616df2db22b7eee64bb842cc38b18897831ad36
Robert Konicar <xkonicar@fi.muni.cz>
2024-11-22 21:58:24 +0100

    test: Add svf test with global struct.

---
+++ test/analysis/basic_c_tests/global-call-struct.c
@@ -0,0 +1,42 @@
+/*
+ * Global variables test.
+ * Author: Sen Ye
+ * Date: 07/05/2014
+ */
+
+// RUN: %emit-llvm -o - %s | %llvm-to-mlir -o - | %potato-opt --llvm-ir-to-potato --points-to-pass="print_lattice=true print_stats=false print_func_stats=false" -o /dev/null
+// RUN: %emit-llvm -o - %s | %llvm-to-mlir -o - | %potato-opt --llvm-ir-to-potato --canonicalize="region-simplify=disabled" --points-to-pass="print_lattice=true print_stats=false print_func_stats=false" -o /dev/null
+// RUN: %emit-llvm -o - %s | %llvm-to-mlir -o - | %potato-opt --llvm-ir-to-potato --steensgaard-points-to-pass="print_lattice=true print_stats=false print_func_stats=false" -o /dev/null
+// RUN: %emit-llvm -o - %s | %llvm-to-mlir -o - | %potato-opt --llvm-ir-to-potato --canonicalize="region-simplify=disabled" --steensgaard-points-to-pass="print_lattice=true print_stats=false print_func_stats=false" -o /dev/null
+
+#include "aliascheck.h"
+
+struct MyStruct {
+	char f0[20];
+	int f1;
+	int* f2;
+};
+
+int x, y;
+struct MyStruct global = {"abcdefg", 20, &x};
+
+void foo(int** pp, int** qq) {
+	*pp = &x;
+	*qq = &y;
+}
+
+void bar(int** pp, int** qq) {
+	*pp = &x;
+	*qq = &x;
+}
+
+int main() {
+	int *p, *q;
+	int **pp, **qq;
+	pp = &p;
+	qq = &q;
+	bar(pp,qq);
+	foo(pp,qq);
+	may_alias(p, q);
+	may_alias(global.f2, *qq);
+}
