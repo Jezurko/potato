2ce70e142637bafdc4c53df381262d6d64f8941d
Robert Konicar <xkonicar@fi.muni.cz>
2024-11-22 22:19:26 +0100

    test: Add more globals tests.

---
+++ test/analysis/basic_c_tests/global-simple.c
@@ -0,0 +1,25 @@
+/*
+ * Global variables test.
+ * Author: Sen Ye
+ * Date: 03/05/2014
+ */
+
+// RUN: %emit-llvm -o - %s | %llvm-to-mlir -o - | %potato-opt --llvm-ir-to-potato --points-to-pass="print_lattice=true print_stats=false print_func_stats=false" -o /dev/null
+// RUN: %emit-llvm -o - %s | %llvm-to-mlir -o - | %potato-opt --llvm-ir-to-potato --canonicalize="region-simplify=disabled" --points-to-pass="print_lattice=true print_stats=false print_func_stats=false" -o /dev/null
+// RUN: %emit-llvm -o - %s | %llvm-to-mlir -o - | %potato-opt --llvm-ir-to-potato --steensgaard-points-to-pass="print_lattice=true print_stats=false print_func_stats=false" -o /dev/null
+// RUN: %emit-llvm -o - %s | %llvm-to-mlir -o - | %potato-opt --llvm-ir-to-potato --canonicalize="region-simplify=disabled" --steensgaard-points-to-pass="print_lattice=true print_stats=false print_func_stats=false" -o /dev/null
+
+#include "aliascheck.h"
+
+int a_int = 10;
+int* p_int = &a_int;
+int** pp_int = &p_int;
+
+int main() {
+	int b_int = a_int;
+	int* q_int = p_int;
+	int** qq_int = pp_int;
+	must_alias(*qq_int, q_int);
+	must_alias(q_int, &a_int);
+	return 0;
+}
