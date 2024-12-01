0739e60a680f807ab6951736af73d28171a414b4
Robert Konicar <xkonicar@fi.muni.cz>
2024-12-01 15:45:43 +0100

    test: Add malloc tests.

---
+++ test/analysis/potato/malloc.c
@@ -0,0 +1,13 @@
+// RUN: %emit-llvm -o - %s | %llvm-to-mlir -o - | %potato-opt --llvm-ir-to-potato --points-to-pass="print_lattice=true print_stats=false print_func_stats=false" -o /dev/null
+// RUN: %emit-llvm -o - %s | %llvm-to-mlir -o - | %potato-opt --canonicalize="region-simplify=disabled" --llvm-ir-to-potato --points-to-pass="print_lattice=true print_stats=false print_func_stats=false" -o /dev/null
+// RUN: %emit-llvm -o - %s | %llvm-to-mlir -o - | %potato-opt --llvm-ir-to-potato --steensgaard-points-to-pass="print_lattice=true print_stats=false print_func_stats=false" -o /dev/null
+// RUN: %emit-llvm -o - %s | %llvm-to-mlir -o - | %potato-opt --canonicalize="region-simplify=disabled" --llvm-ir-to-potato --steensgaard-points-to-pass="print_lattice=true print_stats=false print_func_stats=false" -o /dev/null
+
+#include <stdlib.h>
+void no_alias(int *p, int *q) { return; }
+
+int main() {
+    int *p = malloc(sizeof(int));
+    int *q = malloc(sizeof(int));
+    no_alias(p,q);
+}
