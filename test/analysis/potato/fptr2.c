10e3b29abd22b02fe56e2156b652ce6b33acd730
Robert Konicar <xkonicar@fi.muni.cz>
2024-12-01 11:18:39 +0100

    test: Add fptr tests.

---
+++ test/analysis/potato/fptr2.c
@@ -0,0 +1,25 @@
+// RUN: %emit-llvm -o - %s | %llvm-to-mlir -o - | %potato-opt --llvm-ir-to-potato --points-to-pass="print_lattice=true print_stats=false print_func_stats=false" -o /dev/null
+// RUN: %emit-llvm -o - %s | %llvm-to-mlir -o - | %potato-opt --canonicalize="region-simplify=disabled" --llvm-ir-to-potato --points-to-pass="print_lattice=true print_stats=false print_func_stats=false" -o /dev/null
+// RUN: %emit-llvm -o - %s | %llvm-to-mlir -o - | %potato-opt --llvm-ir-to-potato --steensgaard-points-to-pass="print_lattice=true print_stats=false print_func_stats=false" -o /dev/null
+// RUN: %emit-llvm -o - %s | %llvm-to-mlir -o - | %potato-opt --canonicalize="region-simplify=disabled" --llvm-ir-to-potato --steensgaard-points-to-pass="print_lattice=true print_stats=false print_func_stats=false" -o /dev/null
+
+void may_alias(int *p, int *q) { return; }
+void must_alias(int *p, int *q) { return; }
+
+int x;
+int *px = &x;
+int *px2;
+int *px3;
+
+void foo() { px2 = &x; }
+void bar() { px3 = &x; }
+
+int main() {
+    void (*fp)();
+    fp = &foo;
+    fp = &bar;
+    fp();
+    must_alias(px, px2);
+    must_alias(px, px3);
+    return 0;
+}
