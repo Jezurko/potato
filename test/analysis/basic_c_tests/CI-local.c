0386b86d7b5eef2dcaf7b625a39d1c2a71aaa654
Robert Konicar <xkonicar@fi.muni.cz>
2024-12-03 18:04:43 +0100

    analysis: Import more svf tests.

---
+++ test/analysis/basic_c_tests/CI-local.c
@@ -0,0 +1,34 @@
+/*
+ * Alias due to context-insensitive
+ * Author: Sen Ye
+ * Date: 06/09/2013
+ */
+
+// RUN: %emit-llvm -o - %s | %llvm-to-mlir -o - | %potato-opt --llvm-ir-to-potato --points-to-pass="print_lattice=true print_stats=false print_func_stats=false" -o /dev/null
+// RUN: %emit-llvm -o - %s | %llvm-to-mlir -o - | %potato-opt --llvm-ir-to-potato --canonicalize="region-simplify=disabled" --points-to-pass="print_lattice=true print_stats=false print_func_stats=false" -o /dev/null
+// RUN: %emit-llvm -o - %s | %llvm-to-mlir -o - | %potato-opt --llvm-ir-to-potato --steensgaard-points-to-pass="print_lattice=true print_stats=false print_func_stats=false" -o /dev/null
+// RUN: %emit-llvm -o - %s | %llvm-to-mlir -o - | %potato-opt --llvm-ir-to-potato --canonicalize="region-simplify=disabled" --steensgaard-points-to-pass="print_lattice=true print_stats=false print_func_stats=false" -o /dev/null
+
+#include "aliascheck.h"
+
+void foo(int *m, int *n)
+{
+	may_alias(m,n);
+}
+
+int main()
+{
+	int *p, *q;
+	int a,b;
+	if (a) {
+		p = &a;
+		q = &b;
+		foo(p,q);
+	}
+	else {
+		p = &b;
+		q = &a;
+		foo(p,q);
+	}
+	return 0;
+}
