14562f201069a7bd733d6369ae7ce2d83f8b6b96
Robert Konicar <xkonicar@fi.muni.cz>
2024-12-04 17:29:08 +0100

    test: Add test for static alloc function modelling.

---
+++ test/analysis/potato/static_alloc.c
@@ -0,0 +1,15 @@
+// RUN: %emit-llvm -o - %s | %llvm-to-mlir -o - | %potato-opt --llvm-ir-to-potato --points-to-pass="print_lattice=true print_stats=false print_func_stats=false" -o /dev/null
+// RUN: %emit-llvm -o - %s | %llvm-to-mlir -o - | %potato-opt --llvm-ir-to-potato --canonicalize="region-simplify=disabled" --points-to-pass="print_lattice=true print_stats=false print_func_stats=false" -o /dev/null
+// RUN: %emit-llvm -o - %s | %llvm-to-mlir -o - | %potato-opt --llvm-ir-to-potato --steensgaard-points-to-pass="print_lattice=true print_stats=false print_func_stats=false" -o /dev/null
+// RUN: %emit-llvm -o - %s | %llvm-to-mlir -o - | %potato-opt --llvm-ir-to-potato --canonicalize="region-simplify=disabled" --steensgaard-points-to-pass="print_lattice=true print_stats=false print_func_stats=false" -o /dev/null
+
+unsigned short int** __ctype_b_loc (void);
+
+void may_alias(unsigned short int **x, unsigned short int ** y) { return; }
+int main() {
+    unsigned short int ** x = __ctype_b_loc();
+    unsigned short int ** y = __ctype_b_loc();
+    may_alias(x, y);
+    return 0;
+}
+
