dd333e1301ed844ba6b73784dfe7d882d9643e4c
Robert Konicar <xkonicar@fi.muni.cz>
2024-12-05 17:38:39 +0100

    test: Add test for function with list of models.

---
+++ test/analysis/potato/fptr-args.c
@@ -0,0 +1,17 @@
+// RUN: %emit-llvm -o - %s | %llvm-to-mlir -o - | %potato-opt --llvm-ir-to-potato --points-to-pass="print_lattice=true print_stats=false print_func_stats=false" -o /dev/null
+// RUN: %emit-llvm -o - %s | %llvm-to-mlir -o - | %potato-opt --canonicalize="region-simplify=disabled" --llvm-ir-to-potato --points-to-pass="print_lattice=true print_stats=false print_func_stats=false" -o /dev/null
+// RUN: %emit-llvm -o - %s | %llvm-to-mlir -o - | %potato-opt --llvm-ir-to-potato --steensgaard-points-to-pass="print_lattice=true print_stats=false print_func_stats=false" -o /dev/null
+// RUN: %emit-llvm -o - %s | %llvm-to-mlir -o - | %potato-opt --canonicalize="region-simplify=disabled" --llvm-ir-to-potato --steensgaard-points-to-pass="print_lattice=true print_stats=false print_func_stats=false" -o /dev/null
+
+#include <inttypes.h>
+
+void must_alias(char * p, char *q);
+
+int main() {
+    char* str = "abc123";
+    char *endptr;
+    int x = strtoimax(str, &endptr, 10);
+    // string literal becomes a global constant in llvm
+    must_alias(endptr, str);
+    return 0;
+}
