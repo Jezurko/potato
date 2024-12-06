add63f49533efba0e5b699d8bb75bef03a9caf63
Robert Konicar <xkonicar@fi.muni.cz>
2024-12-06 22:59:49 +0100

    test: Argc test.

---
+++ test/analysis/potato/argc.c
@@ -0,0 +1,13 @@
+// RUN: %emit-llvm -o - %s | %llvm-to-mlir -o - | %potato-opt --llvm-ir-to-potato --points-to-pass="print_lattice=true print_stats=false print_func_stats=false" -o /dev/null
+// RUN: %emit-llvm -o - %s | %llvm-to-mlir -o - | %potato-opt --canonicalize="region-simplify=disabled" --llvm-ir-to-potato --points-to-pass="print_lattice=true print_stats=false print_func_stats=false" -o /dev/null
+// RUN: %emit-llvm -o - %s | %llvm-to-mlir -o - | %potato-opt --llvm-ir-to-potato --steensgaard-points-to-pass="print_lattice=true print_stats=false print_func_stats=false" -o /dev/null
+// RUN: %emit-llvm -o - %s | %llvm-to-mlir -o - | %potato-opt --canonicalize="region-simplify=disabled" --llvm-ir-to-potato --steensgaard-points-to-pass="print_lattice=true print_stats=false print_func_stats=false" -o /dev/null
+
+void may_alias(char *p, char *q) { return; }
+
+int main(int argc, char **argv) {
+    char *arg1 = *argv;
+    char *arg2 = *argv;
+    may_alias(arg1, arg2);
+    return 0;
+}
