c3fc5431ebdaf08f58a1c2517381d8a08b633d8b
Robert Konicar <rkonicar@mail.muni.cz>
2025-04-09 17:10:01 +0200

    dialect: Add custom printer/parser for pt::BranchOp.

--- lib/dialect/ops.cpp
+++ lib/dialect/ops.cpp
@@ -17,6 +17,66 @@ POTATO_UNRELAX_WARNINGS
 
 using namespace potato::pt;
 
+static mlir::ParseResult parseBranches(
+        mlir::OpAsmParser &parser,
+        mlir::SmallVector< mlir_block *, 2> &succs,
+        mlir::DenseI32ArrayAttr &ranges,
+        mlir::SmallVector< mlir::SmallVector< mlir::OpAsmParser::UnresolvedOperand > > &succ_ops,
+        mlir::SmallVector< mlir::SmallVector< mlir::Type > > &succ_ops_types
+) {
+    mlir_block *succ;
+    mlir::SmallVector< mlir::OpAsmParser::UnresolvedOperand > ops;
+    mlir::SmallVector< mlir::Type > types;
+    mlir::SmallVector< int32_t > sizes;
+    do {
+        if (mlir::failed(parser.parseSuccessor(succ)))
+            return mlir::failure();
+        if (mlir::succeeded(parser.parseOptionalLParen())) {
+            if (mlir::failed(parser.parseOperandList(ops)))
+               return  mlir::failure();
+            if (ops.size() > 0) {
+                if (mlir::failed(parser.parseColonTypeList(types)))
+                    return mlir::failure();
+            }
+            if (mlir::failed(parser.parseRParen()))
+                return mlir::failure();
+        }
+        if (ops.size() != types.size())
+            return mlir::failure();
+        sizes.push_back(ops.size());
+
+        succs.push_back(succ);
+        succ = nullptr;
+
+        succ_ops.push_back(std::move(ops));
+        ops.clear();
+
+        succ_ops_types.push_back(std::move(types));
+        types.clear();
+
+    } while(mlir::succeeded(parser.parseOptionalComma()));
+
+    ranges = mlir::DenseI32ArrayAttr::get(parser.getContext(), sizes);
+
+    return mlir::success();
+}
+
+static void printBranches(
+        mlir::OpAsmPrinter &printer,
+        BranchOp op,
+        mlir::SuccessorRange succs,
+        mlir::ArrayRef< int32_t >,
+        mlir::OperandRangeRange succ_operand_groups,
+        mlir::TypeRangeRange succ_operand_types
+) {
+    auto sep = " ";
+    for (size_t i = 0; i < succs.size(); i++) {
+        printer << sep;
+        printer.printSuccessorAndUseList(succs[i], succ_operand_groups[i]);
+        sep = ", ";
+    }
+};
+
 #define GET_OP_CLASSES
 #include "potato/dialect/Potato.cpp.inc"
 
