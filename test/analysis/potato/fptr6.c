5141ba79c229bbabea4021e2c165d898e34f92e5
Robert Konicar <rkonicar@mail.muni.cz>
2025-06-18 13:35:23 +0200

    test: Add another fptr analysis test.

---
+++ test/analysis/potato/fptr6.c
@@ -0,0 +1,23 @@
+// RUN: %emit-llvm -o - %s | %llvm-to-mlir -o - | %potato-opt --llvm-ir-to-potato --points-to-pass="print_lattice=true print_stats=false print_func_stats=false" -o /dev/null
+// RUN: %emit-llvm -o - %s | %llvm-to-mlir -o - | %potato-opt --llvm-ir-to-potato --canonicalize="region-simplify=disabled" --points-to-pass="print_lattice=true print_stats=false print_func_stats=false" -o /dev/null
+// RUN: %emit-llvm -o - %s | %llvm-to-mlir -o - | %potato-opt --llvm-ir-to-potato --steensgaard-points-to-pass="print_lattice=true print_stats=false print_func_stats=false" -o /dev/null
+// RUN: %emit-llvm -o - %s | %llvm-to-mlir -o - | %potato-opt --llvm-ir-to-potato --canonicalize="region-simplify=disabled" --steensgaard-points-to-pass="print_lattice=true print_stats=false print_func_stats=false" -o /dev/null
+
+void may_alias(int *p, int *q) { return; }
+
+int *foo(int *a, int *b) {
+    if (!a)
+      return b;
+    return a;
+}
+
+int main(void) {
+  int a = 0;
+  int b = 0;
+  int *p;
+  int *(*fp)(int *, int *) = &foo;
+  p = fp(&a, &b);
+  may_alias(p, &a);
+  may_alias(p, &b);
+  return a;
+}
