6e14f240113afb8be421631068c0d8eb08ff45da
Robert Konicar <rkonicar@mail.muni.cz>
2025-10-26 18:48:04 +0100

    analysis: Replace get_deref_lats with get_pointees.

--- include/potato/analysis/andersen.hpp
+++ include/potato/analysis/andersen.hpp
@@ -19,13 +19,7 @@ struct aa_lattice : pt_lattice_base< aa_lattice > {
     change_result set_unknown();
     change_result join(const aa_lattice &);
     change_result insert(lattice_anchor);
-    llvm::SmallVector< aa_lattice * > get_deref_lats(auto &analysis) const {
-        llvm::SmallVector< aa_lattice *> res;
-        res.reserve(pointees.size());
-        for (const auto &anchor : pointees)
-            res.push_back(analysis.template getOrCreate< aa_lattice >(anchor));
-        return res;
-    }
+    const llvm::DenseSet< lattice_anchor> &get_pointees() const { return pointees; }
     void print(llvm::raw_ostream &) const override;
 
 private:
